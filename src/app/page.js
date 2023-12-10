a6492b16988d30f587aafd24c823c861824a7e6c
matheswaaran <matheswaarans@gmail.com>
2023-12-10 20:44:26 +0530

    fix: Add white color for text

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
       />
       <div className="h-screen w-screen flex items-center justify-center">
         <div className="flex flex-col items-center justify-center px-4 py-16 bg-black bg-opacity-60 backdrop-blur-[6px] mx-5">
-          <div className="flex items-center justify-center mb-5">
+          <div className="flex items-center justify-center mb-5 text-white">
             <div className="text-[30px] lg:text-[60px] font-akayaKanadaka">
               Narendra
             </div>
@@ -27,7 +27,7 @@ export default function Home() {
               Aarthi
             </div>
           </div>
-          <div className="text-[20px] lg:text-[32px] text-center font-josefinSans w-full lg:w-2/3">
+          <div className="text-[20px] lg:text-[32px] text-center font-josefinSans w-full lg:w-2/3 text-white">
             Website is in progress and will launch soon, please check back later
           </div>
         </div>
