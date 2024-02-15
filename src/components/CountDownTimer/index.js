81a9032bfdcf4843d652056e410d4ddc5228cb80
matheswaaran <matheswaarans@gmail.com>
2024-02-15 18:54:26 +0530

    fix: Fix dates and timer

--- src/components/CountDownTimer/index.js
+++ src/components/CountDownTimer/index.js
@@ -1,29 +1,28 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import TimerSection from "@/components/CountDownTimer/TimerSection";
-import { getTimerDetails } from "@/utils";
 
 const CountDownTimer = (props) => {
-  const today = new Date();
-  const weedingDate = new Date(2024, 1, 0, 0, 0, 0, 0);
+  // const today = new Date();
+  // const weedingDate = new Date(2024, 1, 0, 0, 0, 0, 0);
 
-  const [counter, setCounter] = useState(getTimerDetails(today, weedingDate));
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter(getTimerDetails(new Date(), weedingDate));
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+  // const [counter, setCounter] = useState(getTimerDetails(today, weedingDate));
+  //
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCounter(getTimerDetails(new Date(), weedingDate));
+  //   }, 1000);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   return (
     <div className="flex items-center justify-between">
-      <TimerSection number={counter.w} range="WEEKS" />
-      <TimerSection number={counter.d} range="DAYS" />
-      <TimerSection number={counter.h} range="HOURS" />
-      <TimerSection number={counter.m} range="MINS" />
-      <TimerSection number={counter.s} range="SECS" />
+      <TimerSection number={0} range="WEEKS" />
+      <TimerSection number={0} range="DAYS" />
+      <TimerSection number={0} range="HOURS" />
+      <TimerSection number={0} range="MINS" />
+      <TimerSection number={0} range="SECS" />
     </div>
   );
 };
